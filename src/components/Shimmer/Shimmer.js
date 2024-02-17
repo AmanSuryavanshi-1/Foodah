ce9f0b920cc90d8ceed0b9b54be5af2ca03b42c6
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-02-17 12:26:49 +0530

    ShimmerUI

--- src/components/Shimmer/Shimmer.js
+++ src/components/Shimmer/Shimmer.js
@@ -3,15 +3,14 @@ import './Shimmer.css'
 const Shimmer = () => {
   return (
     <div className='shimmerContainer'>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
-        <div className="shimmerCard"> </div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
+        <div className="shimmerCard">loading</div>
     </div>
   )
 }
