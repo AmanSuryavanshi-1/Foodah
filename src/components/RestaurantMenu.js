dd186de9e47fcf14402836b4afd7c8462205af60
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-07-01 14:10:19 +0530

    styling completed

--- src/components/RestaurantMenu.js
+++ src/components/RestaurantMenu.js
@@ -16,9 +16,9 @@ const RestaurantMenu = () => {
       <div className="max-w-5xl mx-72 px-4 py-8 ">
       <h1 className="text-4xl font-bold text-primary-light font-serif mb-5">Menu</h1>
 
-        <div className="mainResInfo shadow-primary-light shadow-md rounded-lg border border-primary-yellow p-5 mb-8 flex items-center">
+        <div className="mainResInfo shadow-primary-light shadow-sm rounded-lg border border-primary-yellow p-4 mb-8 flex items-center">
           <img
-            className="w-32 h-26 object-cover rounded mr-4"
+            className="w-32 h-32 object-cover rounded mr-4"
             src={CDN_URL + res?.cloudinaryImageId}
             alt={res?.name}
           />
@@ -32,26 +32,26 @@ const RestaurantMenu = () => {
             </div>
             <div className="flex items-center text-sm text-primary-light mb-1">
               <span>{res?.sla?.slaString}</span>
-              <span className="mx-1 font-extrabold">|</span>
-              <span>{res?.costForTwoMessage}</span>
+              <span className="mx-1">|</span>
+              <span className="font-extrabold font-mono">{res?.costForTwoMessage}</span>
             </div>
           </div>
         </div>
 
-        <div className="grid grid-cols-1 gap-4">
-          <h1 className="text-xl font-bold text-primary-light font-serif ">{recommended?.title}</h1>
+        <div className="grid grid-cols-1 gap-4 mt-12">
+          <h1 className="text-xl font-bold text-primary-light font-serif">{recommended?.title}</h1>
           {recommended?.itemCards.map((item, index) => (
-            <div key={index} className="bg-white shadow-lg rounded-lg overflow-hidden flex items-center p-4">
-              <img className="w-24 h-24 object-cover rounded mr-4" src={CDN_URL + item?.card?.info?.imageId} alt={img1} />
+            <div key={index} className="flex items-center p-4 shadow-xl rounded-lg overflow-hidden">
+              <img className="w-28 h-24 object-cover rounded mr-4" src={CDN_URL + item?.card?.info?.imageId} alt={img1} />
               <div className="flex-1">
                 <div className="flex items-center justify-between mb-2">
-                  <h3 className="text-lg font-semibold text-gray-800">{item?.card?.info?.name}</h3>
-                  <span className="text-lg font-bold text-gray-800">₹{(item?.card?.info?.defaultPrice) / 100}</span>
+                  <h3 className="text-lg font-semibold text-primary-light">{item?.card?.info?.name}</h3>
+                  <span className="text-lg font-bold  text-primary-white">₹{(item?.card?.info?.defaultPrice) / 100}</span>
                 </div>
-                <p className="text-sm text-gray-600 mb-2">{item?.card?.info?.description}</p>
+                <p className="text-sm text-primary-light mb-2 mr-12">{item?.card?.info?.description}</p>
                 <div className="flex items-center justify-between">
                   <span className="text-sm text-green-600">★ {item?.card?.info?.ratings?.aggregatedRating?.rating} ({item?.card?.info?.ratings?.aggregatedRating?.ratingCount})</span>
-                  <button className="bg-green-500 text-white px-3 py-1 rounded hover:bg-green-600">ADD</button>
+                  <button className="bg-primary-yellow text-primary-bgColor shadow-md hover:bg-primary-light transition duration-300 px-3 py-1 rounded">ADD</button>
                 </div>
               </div>
             </div>
