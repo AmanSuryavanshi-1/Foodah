f7e895468cf0365d7c781d1e0f9a6011d6f90b29
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-07-04 13:56:42 +0530

    about styling

--- src/components/NavComponents/About.js
+++ src/components/NavComponents/About.js
@@ -9,10 +9,10 @@ const About = () => {
   return (
     <section className="flex items-center justify-center mx-8 mb-4 h-80vh">
       <div className="w-full max-w-6xl px-4 mx-12">
-        <h5 className="mb-4 text-xs text-center text-gray-500 uppercase">Get To Know</h5>
-        <h2 className="mb-8 font-serif text-3xl font-bold text-center text-primary">About Me</h2>
+        <h5 className="mb-2 text-xs text-center uppercase text-primary-yellow">Get To Know</h5>
+        <h2 className="mb-4 font-serif text-3xl font-bold text-center text-primary-light">About Me</h2>
 
-        <div className="grid items-center grid-cols-1 gap-12 md:grid-cols-2">
+        <div className="grid items-center grid-cols-1 gap-2 md:grid-cols-2">
           <div className="relative flex items-center justify-center ">
             <div className="absolute shadow-lg shadow-primary-light w-72 h-64 bg-primary-yellow rounded-2xl transform transition duration-300 hover:rotate-[-10deg]"></div>
             <div className="w-72 h-64 overflow-hidden transform rotate-[-5deg] duration-300 bg-white shadow-lg rounded-2xl transition hover:rotate-0">
@@ -20,33 +20,39 @@ const About = () => {
             </div>
           </div>
 
-          <div className="about_content">
-            <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
-              <article className="p-6 text-center transition-all bg-gray-100 rounded-lg shadow-lg hover:shadow-xl">
-                <FaAward className="mx-auto mb-2 text-3xl text-yellow-500" />
-                <h5 className="mb-2 text-lg font-semibold">Experience</h5>
-                <small className="text-xs text-gray-500">1+ Years Working</small>
-              </article>
+                <div className="about_content">
+                <div className="grid grid-cols-1 gap-4 md:grid-cols-3">
+                  <article className="p-6 text-center transition-all duration-300 border-2 rounded-2xl shadow-sm shadow-primary-light bg-primary-bgColor border-primary-yellow hover:shadow-sm hover:shadow-primary-light hover:bg-primary-dark transform hover:translate-y-[-10px]">
+                      <FaAward className="mx-auto mb-2 text-3xl text-primary-yellow" />
+                      <h5 className="mb-2 font-serif text-lg font-bold text-primary-light">Experience</h5>
+                      <small className="text-xs text-primary-white">1+ Years Working</small>
+                    </article>
 
-              <article className="p-6 text-center transition-all bg-gray-100 rounded-lg shadow-lg hover:shadow-xl">
-                <FiUsers className="mx-auto mb-2 text-3xl text-yellow-500" />
-                <h5 className="mb-2 text-lg font-semibold">Worked With</h5>
-                <small className="text-xs text-gray-500">2+ Companies</small>
-              </article>
+                    <article className="p-6 text-center transition-all duration-300 border-2 rounded-2xl shadow-sm shadow-primary-light bg-primary-bgColor border-primary-yellow hover:shadow-sm hover:shadow-primary-light hover:bg-primary-dark transform hover:translate-y-[-10px]">
+            <FiUsers className="mx-auto mb-2 text-3xl text-primary-yellow" />
+            <h5 className="mb-2 font-serif text-lg font-bold text-primary-light">Worked With</h5>
+            <small className="text-xs text-primary-white">2+ Companies</small>
+          </article>
 
-              <article className="p-6 text-center transition-all bg-gray-100 rounded-lg shadow-lg hover:shadow-xl">
-                <VscFolderLibrary className="mx-auto mb-2 text-3xl text-yellow-500" />
-                <h5 className="mb-2 text-lg font-semibold">Projects</h5>
-                <small className="text-xs text-gray-500">35+ Completed</small>
-              </article>
-            </div>
-            <Link to="#Contact" className="inline-block px-8 py-3 mt-4 text-center text-white transition duration-300 bg-yellow-500 border rounded-lg hover:bg-yellow-600">
-              Let's Talk
-            </Link>
-          </div>
+          <article className="p-6 text-center transition-all duration-300 border-2 rounded-2xl shadow-sm shadow-primary-light bg-primary-bgColor border-primary-yellow hover:shadow-sm hover:shadow-primary-light hover:bg-primary-dark transform hover:translate-y-[-10px]">
+            <VscFolderLibrary className="mx-auto mb-2 text-3xl text-primary-yellow" />
+            <h5 className="mb-2 font-serif text-lg font-bold text-primary-light">Projects</h5>
+            <small className="text-xs text-primary-white">35+ Completed</small>
+          </article>
         </div>
 
-        <p className="my-8 text-lg leading-relaxed text-gray-600">
+          <div className="flex justify-center mt-8">
+            <Link to="/contact" className="flex items-center justify-center px-8 py-2 transition duration-300 border-2 border-primary-light bg-primary-yellow rounded-xl text-primary-bgColor hover:bg-primary-light">
+              <button className='font-sans text-sm'>Let's Talk</button>
+            </Link>
+          </div>
+          
+      </div>
+
+    </div>
+
+
+        <p className="mx-20 my-6 text-lg leading-7 text-primary-light">
           Hi, I'm a web developer and UI/UX designer with a passion for creating beautiful, functional, and user-centered digital experiences. I am always looking for new and innovative ways to bring my visions to life. I believe that design is about more than just making things look pretty - it's about solving problems and creating intuitive, enjoyable experiences for users. Whether I'm working on a website or a mobile app, I bring my commitment to design excellence and user-centered thinking to every project I work on. I look forward to the opportunity to bring my skills and passion to the next project.
         </p>
       </div>
