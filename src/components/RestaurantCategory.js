694c79f946fcaa9f907e0a4e2d5d4feebd51da89
Aman Suryavanshi <amansuryavanshi2812@gmail.com>
2024-07-25 19:24:40 +0530

    nested category issue fixed

--- src/components/RestaurantCategory.js
+++ src/components/RestaurantCategory.js
@@ -11,26 +11,32 @@ const RestaurantCategory = ({ catData }) => {
         setShowDropdown(!showDropdown); // for toggle feature of drop down menu
     }
     // console.log(catData);
-  return (
-    <div className="mb-8 overflow-hidden rounded-lg shadow-lg bg-primary-grey">
-        <div 
-            className="p-4 transition-colors duration-300 cursor-pointer hover:bg-primary-dark"
-            onClick={handleClick}
-        >
-            <h1 className="flex items-center justify-between font-serif text-lg font-bold sm:text-xl md:text-2xl text-primary-light">
-      
-                {/* header */}
-                <p>{catData?.title}   
-                    <span>  ({catData?.itemCards?.length ? catData?.itemCards.length : catData?.categories?.length || 0})  </span>
-                </p> 
-                <span className="text-2xl sm:text-3xl text-primary-yellow"
-                > {showDropdown ? <MdExpandLess /> : <MdExpandMore />} </span>
-            </h1>
+    // Check if catData.categories is an array
+    const nestedCategories = Array.isArray(catData?.categories) ? catData.categories : [];
+
+    return (
+        <div className="mb-8 overflow-hidden rounded-lg shadow-lg bg-primary-grey">
+            <div 
+                className="p-4 transition-colors duration-300 cursor-pointer hover:bg-primary-dark"
+                onClick={handleClick}
+            >
+                <h1 className="flex items-center justify-between font-serif text-lg font-bold sm:text-xl md:text-2xl text-primary-light">
+                    <p>
+                        {catData?.title}   
+                        <span>({catData?.itemCards?.length || nestedCategories.length || 0})</span>
+                    </p> 
+                    <span className="text-2xl sm:text-3xl text-primary-yellow">
+                        {showDropdown ? <MdExpandLess /> : <MdExpandMore />}
+                    </span>
+                </h1>
+            </div>
+            {showDropdown && <RestaurantItemList itemCardsData={catData?.itemCards} />}
+            {/* Map through nested categories if they exist */}
+            {showDropdown && nestedCategories.map((i, index) => (
+                <RestaurantItemList key={index} itemCardsData={i.itemCards} />
+            ))}
         </div>
-    { showDropdown && <RestaurantItemList itemCardsData={catData?.itemCards} />}
-    { showDropdown && <RestaurantItemList itemCardsData={catData?.categories} />}
-    </div>
-  )
+    )
 }
 
 export default RestaurantCategory
